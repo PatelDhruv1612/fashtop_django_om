98a2e1b45e1618f6d75fbe48d0c016b6a6a49f3e
Dhruv Patel <dhruvpatel12162001@gmail.com>
2024-02-14 15:49:21 +0530

    one branch added to the  apps.py file

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-# we are in app .pyq
+# we are in app .pygit 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
