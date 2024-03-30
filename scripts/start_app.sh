8c0a2fa704b0b1ea13304aef3383e072f8b923f6
Dhruv Patel <dhruvpatel12162001@gmail.com>
2024-03-30 23:06:12 +0530

    change in start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["13.232.124.3"]/' /home/ubuntu/fashtop_django_om/fashTOP/settings.py
+sed -i 's/\[]/\["3.110.159.165"]/' /home/ubuntu/fashtop_django_om/fashTOP/settings.py
 
 python manage.py migrate 
 # python manage.py makemigrations     
 python manage.py collectstatic
 sudo service gunicorn restart
 sudo service nginx restart
-#sudo tail -f /var/log/nginx/error.log
-#sudo systemctl reload nginx
-#sudo tail -f /var/log/nginx/error.log
-#sudo nginx -t
-#sudo systemctl restart gunicorn
-#sudo systemctl status gunicorn
-#sudo systemctl status nginx
-# Check the status
-#systemctl status gunicorn
-# Restart:
-#systemctl restart gunicorn
-#sudo systemctl status nginx
+sudo tail -f /var/log/nginx/error.log
+sudo systemctl reload nginx
+sudo tail -f /var/log/nginx/error.log
+sudo nginx -t
+sudo systemctl restart gunicorn
+sudo systemctl status gunicorn
+sudo systemctl status nginx
+Check the status
+systemctl status gunicorn
+Restart:
+systemctl restart gunicorn
+sudo systemctl status nginx
