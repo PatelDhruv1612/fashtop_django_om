b2cebad2ce3d70b4dba25f1d39a69af8d9200167
Dhruv Patel <dhruvpatel12162001@gmail.com>
2024-04-13 22:05:13 +0530

    add one coment

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -6,7 +6,7 @@ sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo cp /home/ubuntu/fashtop_django_om/nginx/nginx.conf /etc/nginx/sites-available/fashTOP
 sudo ln -s /etc/nginx/sites-available/fashTOP /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+#--sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
