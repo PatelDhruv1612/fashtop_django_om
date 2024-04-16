37916d17aa9ffc1d1948056cf2acf8d563590d95
Dhruv Patel <dhruvpatel12162001@gmail.com>
2024-04-16 14:37:13 +0530

    change ip in stat_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["13.201.51.52"]/' /home/ubuntu/fashtop_django_om/fashTOP/settings.py
+sed -i 's/\[]/\["13.201.49.123"]/' /home/ubuntu/fashtop_django_om/fashTOP/settings.py
 
 python manage.py migrate 
 # python manage.py makemigrations     
